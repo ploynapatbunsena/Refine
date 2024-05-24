ee1a4700c9568ec81e4c45983903a0c881aaef5d
ploynapatbunsena <ploynapat.bsn@gmail.com>
2024-05-24 13:29:42 +0700

    Home setup for upcomming events

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,16 @@
 import UpcomingEvents from "./home/upcoming-events";
 import DealChart from "./home/deal-chart";
+import UpcomingEventsSkeleton from './skeleton/upcoming-events';
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
 
-export { UpcomingEvents, DealChart};
+export { 
+    UpcomingEvents, 
+    DealChart, 
+    UpcomingEventsSkeleton,
+    AccordionHeaderSkeleton,
+    KanbanColumnSkeleton,
+    ProjectCardSkeleton,
+    LatestActivitiesSkeleton};
